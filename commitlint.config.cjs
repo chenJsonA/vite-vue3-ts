8be9734f129b6c27c1d9de90daa55f981885841f
geekchen <457666575@qq.com>
2024-05-11 14:39:46 +0800

    chore: 提交husky + lint-stage + prettier配置

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -32,10 +32,10 @@ const type = [
   'revert',
   'style',
   'test',
-  'update',
+  'update'
 ]
 module.exports = {
-  ignores: [(commit) => commit.includes('init')],
+  ignores: [commit => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-empty': [2, 'never'], // <type> 不能为空
@@ -47,6 +47,6 @@ module.exports = {
     'subject-case': [0],
     'header-max-length': [2, 'always', 72], // header最大72个字符
     'body-leading-blank': [1, 'always'], // body上面要有换行
-    'footer-leading-blank': [1, 'always'], // footer上面要有换行
-  },
+    'footer-leading-blank': [1, 'always'] // footer上面要有换行
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/vite-vue3-ts/',
+  base: '/vite-vue3-ts/',
   build: {
     outDir: 'build'
   },
-  server:{
+  server: {
     host: '0.0.0.0',
     port: 3000
   }
